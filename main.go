2e7e5f3197beeec4fe029992500342fc1149e309
cotabas <cotabas@gmail.com>
2023-11-12 10:25:26 -0800

    entry input added

--- main.go
+++ main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("hey")
-}
 
+  var service string
+  var username string
+  var password string
+
+
+  fmt.Print("Service: ")
+  fmt.Scan(&service)
+
+  fmt.Print("Username: ")
+  fmt.Scan(&username)
+
+  fmt.Print("Password: ")
+  fmt.Scan(&password)
+
+  fmt.Println("Service   Username   Password")
+  fmt.Printf("%s %s %s", service, username, password)
+}
